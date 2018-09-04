2d1ce024ced9e49fd56479b1bea57edc107ec747
Zack Jackson <z.jackson58@gmail.com>
2018-09-03 21:46:58 -0400

    Get the input data

--- app.js
+++ app.js
@@ -13,7 +13,27 @@ var budgetController = (function() {
  */
 
 var UIController = (function() {
-  // Some code here
+  var DOMStrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputButton: ".add__btn"
+  };
+  return {
+    getInput: function() {
+      return {
+        // Income or expense -- ".inc", ".exp"
+        type: document.querySelector(DOMStrings.inputType).value,
+        // Budget item description
+        description: document.querySelector(DOMStrings.inputDescription).value,
+        // Dollar value of budget item
+        value: document.querySelector(DOMStrings.inputValue).value
+      };
+    },
+    getDOMStrings: function() {
+      return DOMStrings;
+    }
+  };
 })();
 
 /**
@@ -22,18 +42,21 @@ var UIController = (function() {
  */
 
 var controller = (function(budgetCtrl, UICtrl) {
+  var DOM = UICtrl.getDOMStrings();
   var ctrlAddItem = function() {
     // 1. Get the field input data
+    var input = UICtrl.getInput();
+    console.log(input);
     // 2. Add the item to the budget controller
     // 3. Add the item to the UI
     // 4. Calculate the budget
     // 5. Display the budget on UI
-
-    console.log("It works.");
   };
 
   /* Click the check button event listener */
-  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+  document
+    .querySelector(DOM.inputButton)
+    .addEventListener("click", ctrlAddItem);
 
   /* Press return/enter key event listener */
   document.addEventListener("keypress", function(e) {
